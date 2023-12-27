4b4363599d7a58f499e2c188bb48a3b27cc0277d
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-12-27 17:22:49 -0500

    add missed files

---
+++ src/lib/components/charts-container.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import { onDestroy, onMount } from 'svelte';
+    import { queryCoingecko } from '$lib/utils/coingecko-utils';
+
+    import type { CoinMarketData } from '$lib/types/coingecko-types';
+
+    let marketData: CoinMarketData[] = [];
+    let timeout: null | ReturnType<typeof setTimeout> = null;
+
+    const coinIds = [
+        'bittensor',
+        'bitcoin',
+        'ethereum',
+        'eos',
+        'monero',
+        'telos',
+        'staked-tlos',
+        'tether',
+        'usd-coin',
+        'binancecoin',
+        'solana',
+        'matic-network',
+        'avalanche-2',
+        'litecoin',
+        'shiba',
+    ];
+
+    async function fetchCoinsMarketData() {
+        const response = await queryCoingecko('/coins/markets', {
+            vs_currency: 'usd',
+            ids: coinIds.join(','),
+        });
+        let data = await response.json();
+        const bittensorIndex = data.findIndex((d: CoinMarketData) => d.id === 'bittensor');
+
+        // ensure bittensor is first 😇
+        if (bittensorIndex !== 0) {
+            const bittensor = data.splice(bittensorIndex, 1);
+            data = bittensor.concat(data);
+        }
+
+        marketData = data;
+    }
+
+    onMount(() => {
+        // eztodo include coingecko attribution
+        // eztodo: add loading state
+
+        fetchCoinsMarketData();
+
+        // refresh data every 10 seconds
+        timeout = setInterval(fetchCoinsMarketData, 10000);
+    });
+
+    onDestroy(() => {
+        if (timeout) {
+            clearInterval(timeout);
+        }
+    });
+</script>
+
+{#each marketData as data}
+    <div>
+        {data.name}
+    </div>
+{/each}
